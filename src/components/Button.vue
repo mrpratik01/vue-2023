c2403e9786367f1e1b228de2cf02e00c88307a65
pratik bhattarai <bhattaraipratik663@gmail.com>
2023-09-05 21:51:17 +0545

    done

---
+++ src/components/Button.vue
@@ -0,0 +1,33 @@
+<template>
+    <button @click="onClick()" :style="{backgroundColor: color}" class="btn">
+        {{ title }}
+
+    </button>
+</template>
+
+<script>
+
+export default {
+    name: 'Button',
+    props: {
+        title: String,
+        color: String,
+
+    }, 
+
+
+    methods: {
+        onClick() {
+            console.log('clicked');
+        }
+    }
+   
+}
+
+
+
+</script>
+
+
+
+
---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button color="green" title="Add"></Button>
+  </header>
+</template>
+
+<script>
+import Button from "./Button";
+
+export default {
+  name: "Header",
+  props: {
+    title: {
+      type: String,
+    },
+  },
+
+  components: {
+    Button,
+  },
+
+
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
